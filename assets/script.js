01863074e5de0413733b2c2fd2893fa325d3eaa5
Luigi Ricardo da Silva <luigi.ricardo123@gmail.com>
2026-01-27 11:20:54 -0300

    Atualiza seletores de botões e refatora estilos para melhorar a responsividade e a navegação do portfólio

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,19 @@
-const buttons = document.querySelectorAll('.sidebar button');
+const buttons = document.querySelectorAll('nav button');
 const sections = document.querySelectorAll('.section');
 
 //Script responsible for handling visibility changes and navigation when buttons are clicked.
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         const target = button.dataset.section;
+    
+    buttons.forEach(btn => btn.classList.remove('active'));
 
     sections.forEach(section => {
         section.classList.remove('active');
     });
 
+            button.classList.add('active');
+            
     document.getElementById(target).classList.add('active');
     });
 });
